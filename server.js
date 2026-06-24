58ee70bdebc4b9cece84e270aef0a7d94c7037ac
Justin Bouffard <jbouffard@nwaretech.com>
2026-06-24 15:45:05 -0400

    Trying out basic functionality of HTMX

--- server.js
+++ server.js
@@ -7,6 +7,124 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
+// Handle GET request to /users endpoint
+app.get("/users", async (req, res) => {
+  const limit = +req.query.limit || 10;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users?_limit=${limit}`,
+  );
+  const users = await response.json();
+
+  res.send(`
+    <h1 class="text-2xl font-bold my-4">Users</h1>
+    <ul>
+      ${users.map((user) => `<li>${user.name}</li>`).join("")}
+    </ul>
+  `);
+});
+
+// Hanldle POST request for temp conversion
+app.post("/convert", (req, res) => {
+  setTimeout(() => {
+    const fahrenheit = parseFloat(req.body.fahrenheit);
+    const celsius = (fahrenheit - 32) * (5 / 9);
+
+    res.send(`
+        <p>${fahrenheit}°F is equal to ${celsius}°C</p>
+    `);
+  }, 2000);
+});
+
+let counter = 0;
+
+// Handle GET request to the /poll endpoint
+app.get("/poll", (req, res) => {
+  counter++;
+
+  let result = "";
+
+  for (let i = 0; i < counter; i++) {
+    result += `<li>Poll count: ${i + 1}</li>`;
+  }
+
+  res.send(result);
+});
+
+let currentTemperature = 20;
+
+// Handle GET request for weather
+app.get("/get-temperature", (req, res) => {
+  currentTemperature += Math.random() * 2 - 1; // Randomly change temperature by -1 to +1
+  res.send(currentTemperature.toFixed(2) + "°C");
+});
+
+const contacts = [
+  { name: "Alice", email: "alice@example.com" },
+  { name: "Bob", email: "bob@example.com" },
+  { name: "Charlie", email: "charlie@example.com" },
+  { name: "David", email: "david@example.com" },
+  { name: "Eve", email: "eve@example.com" },
+];
+
+// Handle POST request for contact search
+app.post("/search", (req, res) => {
+  const searchTerm = req.body.search.toLowerCase();
+
+  if (!searchTerm) {
+    return res.send("<tr></tr>");
+  }
+
+  const searchResults = contacts.filter((contact) => {
+    const name = contact.name.toLowerCase();
+    const email = contact.email.toLowerCase();
+    return name.includes(searchTerm) || email.includes(searchTerm);
+  });
+
+  setTimeout(() => {
+    const searchResultsHtml = searchResults
+      .map(
+        (contact) => `
+        <tr>
+            <td><div class="my-4 p-2">${contact.name}</div></td>
+            <td><div class="my-4 p-2">${contact.email}</div></td>
+        </tr>`,
+      )
+      .join("");
+    res.send(searchResultsHtml);
+  }, 1000);
+});
+
+// Handle POST request for contact search from jsonplaceholder
+app.post("/search/api", async (req, res) => {
+  const searchTerm = req.body.search.toLowerCase();
+
+  if (!searchTerm) {
+    return res.send("<tr></tr>");
+  }
+
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const contacts = await response.json();
+
+  const searchResults = contacts.filter((contact) => {
+    const name = contact.name.toLowerCase();
+    const email = contact.email.toLowerCase();
+    return name.includes(searchTerm) || email.includes(searchTerm);
+  });
+
+  setTimeout(() => {
+    const searchResultsHtml = searchResults
+      .map(
+        (contact) => `
+        <tr>
+            <td><div class="my-4 p-2">${contact.name}</div></td>
+            <td><div class="my-4 p-2">${contact.email}</div></td>
+        </tr>`,
+      )
+      .join("");
+    res.send(searchResultsHtml);
+  }, 1000);
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
